ed8f8f1fc7776d456cb64539a0a494fb8fc5147f
Feyza Ozen <feyzaozen2000@gmail.com>
2022-07-25 14:26:33 +0300

    Change

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function Question(text,options,answer){
 }
 
 Question.prototype.checkAnswer = function(ans){
-    return ans == this.dogruCevap;
+    return ans === this.answer;
 }
 
 let questions = [
@@ -30,13 +30,78 @@ const quiz = new Quiz(questions);
 
 
 
-document.querySelector(".start-btn").addEventListener("click",function(){
-    if(quiz.questions.length != quiz.index){
-        console.log(quiz.getQuestion());
-        quiz.index += 1;
-    } else {
-        console.log("enddd");
-    }
+document.querySelector(".btn_start").addEventListener("click",function(){
+    document.querySelector(".quiz_box").classList.add("active")
+    showQuestion(quiz.getQuestion());
+    document.querySelector(".next-btn").classList.remove("show");
+     
+    
 })
 
+document.querySelector(".next-btn").addEventListener("click",function(){
+    if(quiz.questions.length != quiz.index +1){
+        quiz.index += 1;
+        showQuestion(quiz.getQuestion());
+        document.querySelector(".next-btn").classList.add("show");
+        
+    } else {
+        console.log("endd");
+    }
+});
+
+
+const option_list = document.querySelector(".option_list");
+const correctIcon = '<div class = "icon"> <i class = "fas fa-check" > </i> </div>';
+const incorrectIcon = '<div class = "icon"> <i class = "fas fa-times" > </i> </div>';
+
+function showQuestion(soru){
+    let question =  `<span> ${soru.text}</span> `;
+    let options = '';
+
+    for(let cevap in soru.options){
+        options += 
+
+            `
+            <div class = "option p-2 my-2">
+                <span> <b>${cevap}</b>: ${soru.options[cevap]} </span>
+            </div>
+            `;
+    }
+    
+    document.querySelector(".question_text").innerHTML = question;
+
+    option_list.innerHTML = options;
+
+    const option = option_list.querySelectorAll(".option");
+    
+    for(let opt of option){
+        opt.setAttribute("onclick","optionSelected(this)");
+       
+    }
+   
+    
+}
+
+
+function optionSelected(option){
+    
+    let cevap = option.querySelector("span b").textContent;
+    
+    let soru = quiz.getQuestion();
+ 
+    if(soru.checkAnswer(cevap)){
+        option.classList.add("correct");
+        option.insertAdjacentHTML("beforeend",correctIcon);
+    } else {
+        option.classList.add("incorrect");
+        option.insertAdjacentHTML("beforeend",incorrectIcon);
+        
+    }
+
+    for(let i=0; i< option_list.children.length; i++){
+        option_list.children[i].classList.add("disabled");
+    }
+
+    document.querySelector(".next-btn").classList.add("show");
+}
 
